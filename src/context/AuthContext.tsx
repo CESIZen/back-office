cce38ef1cc5d4ea6d9e43cf4be5c08e50804d8c0
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-05-03 22:35:36 +0200

    feat: Ajout de l'auth

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+import { login } from "@/services/AuthService";
+
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  roleId: number;
+}
+interface AuthContextType {
+  user: User | null;
+  login: (credentials: { email: string; password: string }) => Promise<void>;
+  logout: () => void;
+  isAuthenticated: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = async (credentials: { email: string; password: string }) => {
+    const response = await login(credentials);
+    console.log("response handleLogin", response);
+    if (response.access_token) {
+      if (response.user && response.user.roleId !== 1) {
+        throw new Error("Seuls les administrateurs peuvent se connecter");
+      }
+      localStorage.setItem("userToken", response.token);
+      setUser(response.user);
+    } else {
+      throw new Error("Échec de la connexion");
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("userToken");
+    setUser(null);
+  };
+
+  const isAuthenticated = !!localStorage.getItem("userToken");
+
+  return (
+    <AuthContext.Provider value={{ user, login: handleLogin, logout: handleLogout, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth doit être utilisé dans un AuthProvider");
+  }
+  return context;
+};
