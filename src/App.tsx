4f039f70929865da7f98e2f15b6ea37363001a1a
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-05-03 19:11:47 +0200

    feat: Ajout sidebar shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import Layout from "./components/Layout"
+
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* ajoute d'autres routes ici */}
+        </Routes>
+      </Layout>
     </Router>
   )
 }
